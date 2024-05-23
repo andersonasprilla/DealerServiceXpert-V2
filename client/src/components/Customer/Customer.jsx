ca23cc1097e537705a462859a3944ce7a1269680
Anderson Asprilla <anderson.asprilla@gmail.com>
2024-05-23 13:46:08 -0400

    added spacing

--- client/src/components/Customer/Customer.jsx
+++ client/src/components/Customer/Customer.jsx
@@ -11,17 +11,26 @@ const customers = [
       priority: "Drop Off",
       status: "Checked In",
     },
+    {
+      hatNumber: 'T6980',
+      repairOrder: '896322',
+      openedAt: '2d 4h',
+      customerName: 'John Doe',
+      vehicle: 'Pilot 2024',
+      contact: '(954)-325-9966',
+      priority: "Drop Off",
+      status: "Checked In",
+    },
     
     
     // More customers...
   ]
   
   const Customer = () => {
-    return (
-      <div className=" shadow-lg rounded-3xl">
+    return (   
         <div className="mt-8 flow-root">
           {customers.map((customer, index) => (
-            <div key={index} className="  p-5  whitespace-nowrap flex min-w-full sm:px-6 lg:px-8 h-auto space-x-6 text-left font-medium sm:pl-0">
+            <div key={index} className="  p-5  whitespace-nowrap flex min-w-full sm:px-6 lg:px-8 h-auto space-x-6 text-left font-medium sm:pl-0 mb-3 shadow-lg rounded-3xl">
               <div> 
                     <div className="  mt-1 text-gray-500 ">Hat</div>
                     <div className="mt-2 text-gray-900 ">{customer.hatNumber}</div>
@@ -53,7 +62,6 @@ const customers = [
             </div>
           ))}
         </div>
-      </div>
     );
   };
 
