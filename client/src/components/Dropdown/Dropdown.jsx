8106a77b06af8719814dd68dcd44a2224dbc863f
Anderson Asprilla <anderson.asprilla@gmail.com>
2024-05-23 13:26:29 -0400

    update customer component

--- client/src/components/Dropdown/Dropdown.jsx
+++ client/src/components/Dropdown/Dropdown.jsx
@@ -1,5 +1,5 @@
 import { Fragment } from 'react';
-import { Menu, Transition, Portal } from '@headlessui/react';
+import { Menu, Transition } from '@headlessui/react';
 import { ChevronDownIcon } from '@heroicons/react/20/solid';
 
 function classNames(...classes) {
@@ -25,8 +25,8 @@ const Dropdown = () => {
           leaveFrom="transform opacity-100 scale-100"
           leaveTo="transform opacity-0 scale-95"
         >
-          <Portal className>
-            <Menu.Items className="absolute right-0 z-10 mt-2 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          
+            <Menu.Items className="absolute z-10 mt-2  rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
               <div className="py-1">
                 <Menu.Item>
                   {({ active }) => (
@@ -69,7 +69,7 @@ const Dropdown = () => {
                 </Menu.Item>
               </div>
             </Menu.Items>
-          </Portal>
+          
         </Transition>
       </Menu>
     </div>
--- client/src/components/Table/Table.jsx
+++ client/src/components/Table/Table.jsx
@@ -1,90 +1,60 @@
 import Dropdown from "../Dropdown/Dropdown";
 
 const customers = [
-  {
-    hatNumber: 'T6980',
-    repairOrder: '896322',
-    openedAt: '2d 4h',
-    customerName: 'John Doe',
-    vehicle: 'Pilot 2024',
-    contact: '(954)-325-9966',
-    priority: "Drop Off",
-    status: "Checked In",
-  },
+    {
+      hatNumber: 'T6980',
+      repairOrder: '896322',
+      openedAt: '2d 4h',
+      customerName: 'John Doe',
+      vehicle: 'Pilot 2024',
+      contact: '(954)-325-9966',
+      priority: "Drop Off",
+      status: "Checked In",
+    },
+    
+    
+    // More customers...
+  ]
   
-  // More customers...
-];
-
-const Table = () => {
-  return (
-    <div className="px-4 sm:px-6 lg:px-8 shadow-lg rounded-3xl">
-      <div className="mt-8 flow-root">
-        <div className="-mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div className="inline-block min-w-full align-middle sm:px-6 lg:px-8 h-auto">
-            <table className="min-w-full divide-y divide-gray-300">
-              <tbody className="divide-y divide-gray-200 bg-white">
-                {customers.map((customer) => (
-                  <tr key={customer.hatNumber}>
-                    <td className="whitespace-nowrap py-5 pl-4 pr-3 text-sm sm:pl-0">
-                      <div className="flex items-center">
-                        <div className="ml-4">
-                          <div className="mt-1 text-gray-500 ">Hat</div>
-                          <div className="mt-2 font-medium text-gray-900">{customer.hatNumber}</div>
-                        </div>
-                      </div>
-                    </td>
-                    <td className="whitespace-nowrap py-5 pl-4 pr-3 text-sm sm:pl-0">
-                      <div className="flex items-center">
-                        <div className="ml-4">
-                          <div className="mt-1 text-gray-500 ">Repair Order</div>
-                          <div className="mt-2 font-medium text-gray-900">{customer.repairOrder}</div>
-                        </div>
-                      </div>
-                    </td>
-                    <td className="whitespace-nowrap py-5 pl-4 pr-3 text-sm sm:pl-0">
-                      <div className="flex items-center">
-                        <div className="ml-4">
-                          <div className="mt-1 text-gray-500 ">Opened</div>
-                          <div className="mt-2 font-medium text-gray-900">{customer.openedAt}</div>
-                        </div>
-                      </div>
-                    </td>
-                    <td className="whitespace-nowrap py-5 pl-4 pr-3 text-sm sm:pl-0">
-                      <div className="flex items-center">
-                        <div className="ml-4">
-                          <div className="mt-1 text-gray-500 ">Customer Name</div>
-                          <div className="mt-2 font-medium text-gray-900">{customer.customerName}</div>
-                        </div>
-                      </div>
-                    </td>
-                    <td className="whitespace-nowrap py-5 pl-4 pr-3 text-sm sm:pl-0">
-                      <div className="flex items-center">
-                        <div className="ml-4">
-                          <div className="mt-1 text-gray-500 ">Contact</div>
-                          <div className="mt-2 font-medium text-gray-900">{customer.contact}</div>
-                        </div>
-                      </div>
-                    </td>
-                    <td className="whitespace-nowrap py-5 pl-4 pr-3 text-sm sm:pl-0">
-                      <div className="flex items-center">
-                        <div className="ml-4">
-                          <div className="mt-1 text-gray-500 ">Priority</div>
-                          <div className="mt-2 font-medium text-gray-900">{customer.priority}</div>
-                        </div>
-                      </div>
-                    </td>
-                    <td className="whitespace-nowrap px-3 py-5 text-sm text-blue-500 relative">
-                      <Dropdown />
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
+  const Test = () => {
+    return (
+      <div className="px-4 sm:px-6 lg:px-8 shadow-lg rounded-3xl">
+        <div className="mt-8 flow-root">
+          {customers.map((customer, index) => (
+            <div key={index} className=" test-sm py-5 pl-4 pr-3 whitespace-nowrap flex min-w-full sm:px-6 lg:px-8 h-auto space-x-6 text-left font-medium sm:pl-0">
+              <div> 
+                    <div className="  mt-1 text-gray-500 ">Hat</div>
+                    <div className="mt-2 text-gray-900 ">{customer.hatNumber}</div>
+              </div>
+              <div> 
+                    <div className="  mt-1 text-gray-500 ">Repair Order</div>
+                    <div className="mt-2 text-gray-900 ">{customer.repairOrder}</div>
+              </div>
+              <div> 
+                    <div className="  mt-1 text-gray-500">Opened</div>
+                    <div className="mt-2 text-gray-900">{customer.openedAt}</div>
+              </div>
+              <div> 
+                    <div className="  mt-1 text-gray-500">Customer Name</div>
+                    <div className="mt-2 text-gray-900">{customer.customerName}</div>
+              </div>
+              <div> 
+                    <div className="  mt-1 text-gray-500">Contact</div>
+                    <div className="mt-2 text-gray-900">{customer.contact}</div>
+              </div>
+              <div> 
+                    <div className="  mt-1 text-gray-500">Priority</div>
+                    <div className="mt-2 text-gray-900">{customer.priority}</div>
+              </div>
+              <div > 
+                    <Dropdown />
+              </div>
+              
+            </div>
+          ))}
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
 
-export default Table;
+export default Test
